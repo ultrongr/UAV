c30aec0101e888c49cf6fc3ecf82cae1d31c6f06
ultrongr <up1083865@upnet.gr>
2024-04-21 13:43:16 +0300

    2 models can be imported now

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ unlitLine.line_width = 5
 
 
 
-obj_file_path = "models/F52.obj"
+# obj_file_path = "models/F52.obj"
+obj_file_path = "models/v22_osprey.obj"
 
 
 mesh = o3d.io.read_triangle_mesh(obj_file_path)
@@ -34,9 +35,10 @@ mesh.translate(translation_vector)
 centroid = np.mean(mesh.vertices, axis=0)
 print(f"Mesh centroid: {centroid}")
 
-#fit to unit sphere
-scale = float(1.0 / np.max(np.max(mesh.vertices, axis=0) - np.min(mesh.vertices, axis=0)))
+# Scale it down 
+scale = float(1.2 / np.max(np.max(mesh.vertices, axis=0) - np.min(mesh.vertices, axis=0)))
 mesh = mesh.scale(scale, center=centroid)
+
 
 
 
@@ -58,7 +60,7 @@ class Appwindow:
         self.window.add_child(self._scene)
 
         # set mouse and key callbacks
-        # self._scene.set_on_key(self._on_key_pressed)
+        self._scene.set_on_key(self._on_key_pressed)
         # self._scene.set_on_mouse(self._on_mouse_pressed)
 
         # set up camera
@@ -105,7 +107,31 @@ class Appwindow:
 
         self._scene.scene.remove_geometry(name)
 
+    def _on_key_pressed(self, event):
 
+        if not event.type == event.DOWN:
+            return gui.Widget.EventCallbackResult.IGNORED
+
+        # U key 
+        if event.key == 117: 
+                
+            if "unit_sphere" in self.geometries:
+
+                self.sphere_on = not self.sphere_on
+                self._scene.scene.show_geometry("unit_sphere", self.sphere_on)
+                
+                return gui.Widget.EventCallbackResult.HANDLED
+        
+        # M key 
+        if event.key == 109: 
+            print("M key pressed")
+                
+            self.model_on = not self.model_on
+            self._scene.scene.show_geometry("model", self.model_on)
+            
+            return gui.Widget.EventCallbackResult.HANDLED
+        
+        return gui.Widget.EventCallbackResult.IGNORED
 
 def main():
     global mesh
@@ -120,7 +146,7 @@ def main():
         o3d.geometry.TriangleMesh.create_sphere(radius=1.0)).paint_uniform_color(np.array([1,0,0]))
 
 
-    app.add_geometry(unit_sphere, "unit_sphere", visible = False)
+    app.add_geometry(unit_sphere, "unit_sphere", visible = True)
 
 
     app.add_geometry(mesh, "model")
