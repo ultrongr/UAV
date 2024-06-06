a52da71c4d735e28950112542f80a5724525fbb4
ultrongr <up1083865@upnet.gr>
2024-06-06 12:41:53 +0300

    fixed issues

--- main.py
+++ main.py
@@ -63,6 +63,12 @@ class UAV:
             "sphere": None,
             "kdop": None,
         }
+        self.boxes_visibility = {
+            "aabb": False,
+            "chull": False,
+            "sphere": False,
+            "kdop": False,
+        }
 
         self.position = np.mean(self.mesh.vertices, axis=0)
         if position:            
@@ -110,22 +116,22 @@ class UAV:
             if box_name == "kdop":
                 
                 box.move_by(dist)
-                self.remove_kdop()
-                self.scene.addShape(box, self.name+"_kdop")
-                self.boxes["kdop"] = box
+                if self.boxes_visibility[box_name]:
+                    self.remove_kdop()
+                    self.create_kdop(kdop_number)
             
             elif box_name == "aabb":
                 box.translate(dist)
-                self.remove_aabb()
-                self.scene.addShape(box, self.name+"_aabb")
-                self.boxes["aabb"] = box
+                if self.boxes_visibility[box_name]:
+                    self.remove_aabb()
+                    self.create_aabb()
             
             elif box_name == "chull":
                 for vertex in box._shape.vertices:
                     vertex += dist
-                self.remove_convex_hull()
-                self.scene.addShape(box, self.name+"_chull")
-                self.boxes["chull"] = box
+                if self.boxes_visibility[box_name]:
+                    self.remove_convex_hull()
+                    self.create_convex_hull()
                     
     
     def move_by(self, dist:np.ndarray):
@@ -163,13 +169,19 @@ class UAV:
         sphere = Sphere3D(p=self.position, radius=radius, resolution=resolution)
         self.scene.addShape(sphere, self.name+"_sphere")
         self.boxes["sphere"] = sphere
+        self.boxes_visibility["sphere"] = True
     
     def remove_sphere(self):
         self.scene.removeShape(self.name+"_sphere")
-        self.boxes["sphere"] = None
+        self.boxes_visibility["sphere"] = False
 
     def create_aabb(self):
         "Create an axis-aligned bounding box around the UAV"
+
+        if self.boxes["aabb"]:
+            self.boxes_visibility["aabb"] = True
+            self.scene.addShape(self.boxes["aabb"], self.name+"_aabb")
+            return
 
 
         min_x, min_y, min_z = np.min(self.mesh.vertices, axis=0)
@@ -177,13 +189,20 @@ class UAV:
         box = Cuboid3D(p1=[min_x, min_y, min_z], p2=[max_x, max_y, max_z], color=Color.GREEN, filled=False)
         self.scene.addShape(box, self.name+"_aabb")
         self.boxes["aabb"] = box
+        self.boxes_visibility["aabb"] = True
     
     def remove_aabb(self):
         self.scene.removeShape(self.name+"_aabb")
-        self.boxes["aabb"] = None
+        self.boxes_visibility["aabb"] = False
 
     def create_convex_hull(self):
         "Create a convex hull around the UAV"
+
+        if self.boxes["chull"]:
+            print("test1")
+            self.boxes_visibility["chull"] = True
+            self.scene.addShape(self.boxes["chull"], self.name+"_chull")
+            return
 
 
         
@@ -201,230 +220,33 @@ class UAV:
         hull_mesh._shape.triangles=o3d.utility.Vector3iVector(hull.triangles)
         self.scene.addShape(hull_mesh, self.name+"_chull")
         self.boxes["chull"] = hull_mesh
+        self.boxes_visibility["chull"] = True
         print(f"Hull: {time.time()-start:.2f}s")
 
     def remove_convex_hull(self):
         self.scene.removeShape(self.name+"_chull")
-        self.boxes["chull"] = None
+        self.boxes_visibility["chull"] = False
     
     def create_kdop(self, k:int):
         "Create a k-dop around the UAV"
 
-        self.boxes["kdop"] = Kdop(self.mesh.vertices, k, self.scene, name = self.name+"_kdop")
-        return
-        implemented_values = [6, 14, 26]
-        if k not in implemented_values:
-            print(f"Only {implemented_values} are implemented")
+        if not self.boxes.get("kdop"):
+            vertices = self.mesh.vertices
+            kdop = Kdop(vertices, k, self.scene, self.name + "_kdop", self)
+            self.scene.addShape(kdop, self.name+"_kdop")
+            self.boxes["kdop"] = kdop
+            self.boxes_visibility["kdop"] = True
+        else:
+            self.boxes_visibility["kdop"] = True
+            self.scene.addShape(self.boxes["kdop"], self.name+"_kdop")
+            # for i, face in enumerate(self.boxes["kdop"]._polygons):
+            #     self.scene.addShape(face, self.name+"_kdop_face_"+str(i))
             return
-        if k == 6:
-            self.create_6dop()
-            return
-        if k == 14:
-            self.create_14dop()
-            return
-        
+
+
         
 
 
-    def create_6dop(self):
-        "Create a 6-dop around the UAV"
-
-        min_x, min_y, min_z = np.min(self.mesh.vertices, axis=0)
-        max_x, max_y, max_z = np.max(self.mesh.vertices, axis=0)
-        # box = Cuboid3D(p1=[min_x, min_y, min_z], p2=[max_x, max_y, max_z], color=Color.GREEN, filled=False)
-        # self.scene.addShape(box, self.name+"_kdop")
-        # self.boxes["kdop"] = box
-        faces = [
-            [[min_x, min_y, min_z], [max_x, min_y, min_z], [max_x, max_y, min_z], [min_x, max_y, min_z]],
-            [[min_x, min_y, min_z], [min_x, max_y, min_z], [min_x, max_y, max_z], [min_x, min_y, max_z]],
-            [[min_x, min_y, min_z], [min_x, min_y, max_z], [max_x, min_y, max_z], [max_x, min_y, min_z]],
-            [[max_x, max_y, min_z], [max_x, min_y, min_z], [max_x, min_y, max_z], [max_x, max_y, max_z]],
-            [[max_x, max_y, min_z], [max_x, max_y, max_z], [min_x, max_y, max_z], [min_x, max_y, min_z]],
-            [[max_x, max_y, max_z], [max_x, min_y, max_z], [min_x, min_y, max_z], [min_x, max_y, max_z]],
-        ]
-        polygons = []
-        for face in faces:
-            polygon = ConvexPolygon3D(np.array(face), color=Color.RED)
-            polygons.append(polygon)
-        kdop = Polyhedron3D(polygons, color=Color.RED, name = self.name+"_kdop")
-        self.scene.addShape(kdop, self.name+"_kdop")
-        self.boxes["kdop"] = kdop
-
-
-    def create_14dop(self):
-        "Create a 14-dop around the UAV"
-
-        import time
-
-        def find_intersection(plane1, plane2, plane3):
-            
-            A = np.array([plane1[:3], plane2[:3], plane3[:3]])
-            b = np.array([-plane1[3], -plane2[3], -plane3[3]])
-            x = np.linalg.solve(A, b)
-            return x
-
-        def check_if_neighbor(corner_dir, face_dir):
-            for i in range(3):
-                if face_dir[i] != 0:
-                    if corner_dir[i]==face_dir[i]:
-                        return True
-            return False
-        
-        def plane_equation_from_point_normal(point, normal):
-            # Normalize the normal vector
-            unit_normal = normal / np.linalg.norm(normal)
-            
-            # Extract components
-            a, b, c = unit_normal
-            x0, y0, z0 = point
-            
-            # Calculate d using the point on the plane
-            d = -(a*x0 + b*y0 + c*z0)
-            
-            # Return the coefficients of the plane equation
-            return [a, b, c, d]
-        
-        directions = [
-            [1, 0, 0],
-            [0, 1, 0],
-            [0, 0, 1],
-            [1, 1, 1],
-            [1, 1, -1],
-            [1, -1, 1],
-            [1, -1, -1],
-
-            [-1, 0, 0],
-            [0, -1, 0],
-            [0, 0, -1],
-            [-1, 1, 1],
-            [-1, 1, -1],
-            [-1, -1, 1],
-            [-1, -1, -1],
-
-        ]
-
-        start = time.time()
-
-        points = []
-        
-        directions_to_vertices = {} # Mapping the each direction to the furthest vertex in that direction
-        for direction in directions[:7]: # The other half will be the min vertices
-            max_val = -np.inf
-            max_vertex = None
-            min_val = np.inf
-            min_vertex = None
-            for vertex in self.mesh.vertices:
-                val = np.dot(vertex, direction)
-                if val > max_val:
-                    max_val = val
-                    max_vertex = vertex
-                if val < min_val:
-                    min_val = val
-                    min_vertex = vertex
-            directions_to_vertices[tuple(direction)] = max_vertex
-            directions_to_vertices[tuple(np.array(direction)*-1)] = min_vertex
-        
-
-        triangles = [] # Finding the triangles that are formed by the intersection of the corner planes with the faces
-        for dir in directions_to_vertices.keys():
-            if 0 in dir: # Only going for corners
-                continue
-
-            v = directions_to_vertices[dir] # Furthest vertex in the direction
-            plane = plane_equation_from_point_normal(v, dir) # Plane equation of the corner
-            face_planes = [] # Planes of the faces that are neighbors of the corner
-            for dir2 in directions_to_vertices.keys():
-                if not 0 in dir2: # Only going for faces
-                    continue
-                # Check if the corner is a neighbor of the face
-                if not check_if_neighbor(dir, dir2):
-                    continue
-                plane2 = plane_equation_from_point_normal(directions_to_vertices[dir2], dir2)
-                face_planes.append(plane2)
-
-            temp_points=[] # To avoid duplicate points
-            for plane2 in face_planes:
-                for plane3 in face_planes:
-                    if plane2 == plane3:
-                        continue
-                    point = find_intersection(plane, plane2, plane3) # Intersection of the corner with the faces
-                    for temp_p in temp_points:
-                        if np.linalg.norm(temp_p - point) < 0.001: # Dont add duplicate points
-                            break
-                    else:
-                        points.append(point)
-                        temp_points.append(point)
-            triangle = Triangle3D(p1=temp_points[0], p2=temp_points[1], p3=temp_points[2], color=Color.RED)
-            
-            triangles.append(triangle)
-
-
-        mesh_point = np.mean(self.mesh.vertices, axis=0) # The center of the mesh, used to check if other points are inside the kdop
-        points_to_remove = [] # Points of the triangles that are not inside the kdop
-        valid_points = [] # Points that are inside the kdop
-        for p in points:            
-            for triangle in triangles:
-                if not triangle.points_on_same_side(mesh_point, p): # Check if the point is inside the kdop
-                    points_to_remove.append(p)
-                    break
-            else:
-                valid_points.append(p)
-
-        intersections=[] # The intersections of the triangles that define the kdop
-        for triangle in triangles:
-
-            lines = [Line3D(triangle.p1, triangle.p2), Line3D(triangle.p2, triangle.p3), Line3D(triangle.p3, triangle.p1)] 
-            for other_triangle in triangles:
-                if triangle == other_triangle:
-                    continue
-                
-                for line in lines:
-                    intersection = other_triangle.getLineIntersection(line)
-                    
-                    if intersection is not None:
-                        intersections.append(intersection)
-
-        for point in intersections: # Now valid_points will contain all the points that define the kdop
-            valid_points.append(point)
-
-        
-        dop_faces=[]
-        dop_polygons = []
-
-
-
-        for direction in directions: # For each direction, find the points that are on the plane 
-                                                           # defined by the direction and create a face of the kdop
-            dop_face_points=[]
-            v = directions_to_vertices[tuple(direction)]
-            plane = plane_equation_from_point_normal(v, direction)
-            a, b, c, d = plane
-            for i, p in enumerate(valid_points):
-                
-                if np.abs(a*p[0] + b*p[1] + c*p[2] + d) < 0.01: # Point is on the plane defined by the direction
-                    for other_p in dop_face_points:
-                        if np.linalg.norm(p - other_p) < 0.0001: # Point is already in the face
-                            break
-                    else:
-                        dop_face_points.append(p)
-
-
-          
-            dop_faces.append(dop_face_points)
-            dop_face_points=np.array(dop_face_points)
-            face_polygon = ConvexPolygon3D(dop_face_points, normal=direction,color=Color.RED) # The dace of the kdop corresponding to the direction
-            dop_polygons.append(face_polygon)
-        
-        kdop = Polyhedron3D(dop_polygons, color=Color.RED, name = self.name + "_kdop") # The kdop
-        self.scene.addShape(kdop, self.name+"_kdop")
-        self.boxes["kdop"] = kdop
-
-                
-        
-
-        print(f"14DOP: {time.time()-start:.2f}s")
-
-      
     def remove_kdop(self):
         if not self.boxes.get("kdop"):
             return
@@ -435,7 +257,9 @@ class UAV:
         for i, face in enumerate(kdop._polygons):
             self.scene.removeShape(kdop_name+f"_face_{i}")
         
-        self.boxes["kdop"] = None
+        self.boxes_visibility["kdop"] = False
+        
+        # self.boxes["kdop"] = None
 
 class LandingPad:
     def __init__(self, N:int, scene:Scene3D) -> None:
@@ -484,7 +308,8 @@ class Airspace(Scene3D):
         filename1 = f"models/{model1}.obj"
         filename2 = f"models/{model2}.obj"
         uav1 = UAV(self, filename1, position=[1.5, 1, 0], scale=None)
-        uav2 = UAV(self, filename2, position=[0, 1, 0], scale=None)
+        # uav2 = UAV(self, filename2, position=[0, 1, 0], scale=None)
+        # uav3 = UAV(self, filename1, position=[1.5, 1, 1.5], scale=None)
 
     
     def find_kdop_collisions(self):
@@ -891,28 +716,28 @@ class Airspace(Scene3D):
         if symbol == Key.C:
             print("c")
             for uav in self.uavs.values():
-                if uav.boxes["chull"]:
+                if uav.boxes_visibility["chull"]:
                     uav.remove_convex_hull()
                 else:
                     uav.create_convex_hull()
         
         if symbol == Key.U:
             for uav in self.uavs.values():
-                if uav.boxes["sphere"]:
+                if uav.boxes_visibility["sphere"]:
                     uav.remove_sphere()
                 else:
                     uav.create_sphere(radius=None, resolution=30)
         
         if symbol == Key.B:
             for uav in self.uavs.values():
-                if uav.boxes["aabb"]:
+                if uav.boxes_visibility["aabb"]:
                     uav.remove_aabb()
                 else:
                     uav.create_aabb()
         
         if symbol == Key.K:
             for uav in self.uavs.values():
-                if uav.boxes["kdop"]:
+                if uav.boxes_visibility["kdop"]:
                     # print("removing kdop")
                     uav.remove_kdop()
                 else:
@@ -963,16 +788,35 @@ class Airspace(Scene3D):
 
 class Kdop(Polyhedron3D):
 
-    def __init__(self, vertices:np.ndarray, k:int, scene:Scene3D, name:str) -> None:
+    classes_to_kdop:dict[str, (np.ndarray, "Kdop")] = {}
+
+    def __init__(self, vertices:np.ndarray, k:int, scene:Scene3D, name:str, uav:UAV = None) -> None:
         self.pc_vertices = vertices
         self.k = k
         self.scene = scene
         self.name = name
-        self.aircraft_class = None
+        self.uav = uav
 
+        print("kdop init called")
+        
         self.create_kdop()
 
+        if self.uav:
+            
+            # Kdop.classes_to_kdop[self.uav._class] = (self.uav.position, self)
+            return
+        
+
+        
+
     def create_kdop(self):
+
+        if self.uav:
+            if self.uav._class in Kdop.classes_to_kdop:
+                self.create_from_class()
+                print("created from class")
+                return
+        print(":)")
         if self.k == 6:
             self.create_6dop()
             return
@@ -999,8 +843,8 @@ class Kdop(Polyhedron3D):
             polygons.append(polygon)
         
         super().__init__(polygons, color=Color.RED, name = self.name)
-
-        self.scene.addShape(self, self.name)
+        if self.scene:
+            self.scene.addShape(self, self.name)
     
     def create_14dop(self):
         "Create a 14-dop "
@@ -1165,20 +1009,46 @@ class Kdop(Polyhedron3D):
             dop_face_points=np.array(dop_face_points)
             face_polygon = ConvexPolygon3D(dop_face_points, normal=direction,color=Color.RED) # The face of the kdop corresponding to the direction
             dop_polygons.append(face_polygon)
-        
+        print(f"Creating kdop named {self.name} took {time.time()-start:.2f}s")
         super().__init__(dop_polygons, color=Color.RED, name = self.name) 
-        self.scene.addShape(self, self.name)
+        if self.scene:
+            self.scene.addShape(self, self.name)
 
 
         
         
 
         print(f"14DOP: {time.time()-start:.2f}s")
-       
-    def remove(self):
-        self.scene.removeShape(self.name)     
     
+    def create_from_class(self):
+        import time
 
+        print(f"Creating Kdop from class {self.uav._class}")
+        time1 = time.time()
+        other_uav_position, other_kdop = Kdop.classes_to_kdop[self.uav._class]
+        other_polygons = other_kdop._polygons
+        new_polygons = []
+        for polygon in other_polygons:
+            new_polygon = ConvexPolygon3D(np.array(polygon.vertices), color=Color.RED)
+            new_polygons.append(new_polygon)
+        new_uav_position = self.uav.position
+
+        super().__init__(other_polygons, color=Color.RED, name = self.name)
+        self.move_by(new_uav_position - other_uav_position)
+        if self.scene:
+            self.scene.addShape(self, self.name)
+        print(f"Cached kdop creation time: {time.time()-time1:.2f}s")
+
+        
+
+    def remove(self):
+        if self.scene:
+            self.scene.removeShape(self.name)     
+    
+    def move_by(self, distance: NDArray):
+        super().move_by(distance)
+        if self.uav:
+            Kdop.classes_to_kdop[self.uav._class] = (self.uav.position, self)
     
 
 def main():
